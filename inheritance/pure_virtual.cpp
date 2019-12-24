e61bf878e0456ba9e7d7447f4e22d333f8f60360
Satya Sundar Sahu <tinkusahu.com@gmail.com>
2019-12-25 01:07:36 +0530

    Added Pure Virtual Function
    Signed-off-by: Satya Sundar Sahu <tinkusahu.com@gmail.com>

--- inheritance/pure_virtual.cpp
+++ inheritance/pure_virtual.cpp
@@ -2,17 +2,55 @@
 
 using namespace std;
 
-class shape {
+class shape {			//Abstracr Base class
 public:
 	virtual void draw() = 0; //Pure Virtual Function
+	/*virtual void draw() = 0
+	{
+		cout << "shape init brush pencile" << endl;
+	}*/
 };
 
-class polygon: public shape {
+class polygon: public shape {	//Concrete class
 	void draw()
 	{
+		//shape::draw();
 		cout << "polygon draw by trangulation" << endl;
 	}
 };
+
+class closedconic: public shape {
+	//draw(); //Inherited pure virtual function becz derive class of shape
+};
+
+class quadrilateral: public polygon {
+	void draw()
+	{
+		cout << "Draw quadrilateral line" << endl;
+	}
+};
+
+class triangle: public polygon {
+	void draw()
+	{
+		cout << "Draw Triangle Line" << endl;
+	}
+};
+
+class circle: public closedconic {
+	void draw()
+	{
+		cout << "Circle draw" << endl;
+	}
+};
+
+class ellipse: public closedconic {
+	void draw()
+	{
+		cout << "Draw ellipse" << endl;
+	}
+};
+
 
 int main()
 {
